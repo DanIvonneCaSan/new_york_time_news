dfb74563e553c4cba3ea34d811bae7e9d34fe839
DanivonneCaSan <daniela.castillo.sn@gmial.com>
2018-03-01 06:41:33 -0600

    Adding functions XHR

--- js/app.js
+++ js/app.js
@@ -1,1 +1,54 @@
-ECHO est  activado.
+const form = document.getElementById('search-form');
+const searchField = document.getElementById('search-keyword');
+const responseContainer = document.getElementById('response-container');
+let searchedForText;
+
+form.addEventListener('submit', function (e){
+  e.preventDefault();
+  responseContainer.innerHTML = '';
+  searchedForText = searchField.value;
+  console.log(searchedForText);
+  getNews();
+});
+
+const getNews = () => {
+  const articleRequest = new XMLHttpRequest();
+  articleRequest.open('GET', `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=94be3cf0aead49d785ec7531de81cb60`);
+  articleRequest.onload = addNews;
+  articleRequest.onerror = handleError;
+  articleRequest.send();
+};
+
+const handleError = () => {
+  console.log('Se ha presentado un error');
+};
+
+function addNews(){
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+  const response = data.response;
+  console.log(response);
+  const dataLength = data.response.docs.length;
+  const dataCont = data.response.docs;
+
+  const searchAndPaint = (item, index, array) =>{
+    const article = data.response.docs[index];
+    const ttilte = article.headline.main;
+    const snippet = article.snippet;
+    let li = document.createElement('li');
+    li.className = 'articleClass';
+    li.innerText = snippet;
+    responseContainer.appendChild(li);
+  };
+
+  dataCont.forEach(searchAndPaint);
+  //Estructura para obtener e imprimir un articulo
+  // const article = data.response.docs[0];
+  // const ttilte = article.headline.main;
+  // const snippet = article.snippet;
+  // let li = document.createElement('li');
+  // li.className = 'articleClass';
+  // li.innerText = snippet;
+  //
+  // responseContainer.appendChild(li);
+};
